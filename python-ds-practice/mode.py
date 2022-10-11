7bc49d3ca24b2ac236b4525de3c6ca980701a1d8
root <root@LAPTOP-EEV6NRCI.localdomain>
2022-10-11 17:14:22 -0500

    one long ass set of problems

---
+++ python-ds-practice/mode.py
@@ -0,0 +1,29 @@
+def mode(nums):
+    """Return most-common number in list.
+
+    For this function, there will always be a single-most-common value;
+    you do not need to worry about handling cases where more than one item
+    occurs the same number of times.
+
+        >>> mode([1, 2, 1])
+        1
+
+        >>> mode([2, 2, 3, 3, 2])
+        2
+    """
+    
+     counter = {}
+
+    for num in nums:
+        counter[num] = counter.get(num, 0) + 1
+
+    #this will find the highest value (the most frequent number)
+
+    max_value = max(counter.values())
+
+    #and now we need to see at which index the highest value is at
+
+    for (num, freq) in counter.items():
+        if freq == max_value:
+            return num
+
---
+++ python-ds-practice/number_compare.py
@@ -0,0 +1,18 @@
+def number_compare(a, b):
+    """Report on whether a>b, b>a, or b==a
+    
+        >>> number_compare(1, 1)
+        'Numbers are equal'
+        
+        >>> number_compare(-1, 1)
+        'Second is greater'
+        
+        >>> number_compare(1, -2)
+        'First is greater'
+    """
+    if a>b:
+        return "First Number Is Greater"
+    elif b>a:
+        return "Second Number Is Greater"
+    else:
+        return "Numbers Are Equal"
